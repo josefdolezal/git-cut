ed77f9af51d4a4497827e8203e656a517ae5b881
Josef Dolezal <pepik.ml@gmail.com>
2019-10-08 22:50:48 +0200

    Implement simple branch removing

--- Sources/gitcut/main.swift
+++ Sources/gitcut/main.swift
@@ -1,1 +1,51 @@
-print("Hello, world!")
+import Foundation
+
+func process(path: String, arguments: [String]) throws -> String {
+    let process = Process()
+    let output = Pipe()
+
+    process.currentDirectoryPath = FileManager.default.currentDirectoryPath
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/" + path)
+    process.arguments = arguments
+    process.standardOutput = output
+    try process.run()
+    process.waitUntilExit()
+
+    let outputData = output.fileHandleForReading.readDataToEndOfFile()
+    return String(data: outputData, encoding: .utf8) ?? ""
+}
+
+@discardableResult
+func git(_ cmd: String, arguments: [String] = []) throws -> String {
+    try process(path: "git", arguments: [cmd] + arguments)
+}
+
+let branches = try git("branch")
+    // List branches
+    .split(separator: "\n")
+    // Filter out current branch
+    .filter { !$0.starts(with: "*") }
+    // Remove white spaces
+    .map { $0.trimmingCharacters(in: .whitespaces) }
+
+for branch in branches {
+    var command: String?
+
+    while true {
+        print("Do you want to remove branch '\(branch)'? [N/y]")
+        command = readLine(strippingNewline: true)?.lowercased()
+
+        guard let command = command else {
+            break
+        }
+
+        if command == "y" {
+            try git("branch", arguments: ["-D", branch])
+            break
+        } else if command == "n" || command == "" {
+            print("skipping")
+            break
+        }
+    }
+}
+
